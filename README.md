fd089fe5ea5d142631c85c9b71473cdab0f6b3d5
zarriy <zarriy@outlook.com>
2023-02-12 13:04:57 +0500

    starting files for the project

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 ### About Project
 
-React Project 1.1 - Increment and Decrement Functionality
-
-- [Live Demo](https://incrementappreact.netlify.app/)
-
-![React Test 1.1 - Increment and Decrement screenshot](https://raw.githubusercontent.com/Zarriy/reactTest/react-1.1/src/assets/incrementnumber.png)
-
-This project is developed using Vite + React along with TailwindCSS for basic styling of the layout.
-
-This is a simple app that increment and decrement number upon clicking + or -.
-To avoid reusability of the classes in both button classes, I have added style to the tailwind component layer in the index.css and reuse it in both buttons.
+React Project 1.2 - Filter Functionality
 
 Thanks
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="bg-slate-700 text-center h-screen flex items-center justify-center flex-col">
-      <h1 className="text-white/60 mb-8 text-3xl">React Test 1.1</h1>
-      <div className="w-40 h-40 rounded-full flex items-center justify-center bg-zinc-100	 shadow-lg	mb-10">
-        <h3 className="text-6xl">{count}</h3>
-      </div>
-      <div className="flex space-x-2 justify-center">
-        <button className="button" onClick={() => setCount((prev) => prev + 1)}>
-          +
-        </button>
-        <button
-          className="button disabled:bg-gray-400 disabled:cursor-not-allowed"
-          disabled={count === 0}
-          onClick={() => setCount((prev) => prev - 1)}
-        >
-          -
-        </button>
-      </div>
-    </div>
-  );
+  return <div>Filtered Product Section</div>;
 }
 
 export default App;
