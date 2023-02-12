08d9063ba0a6fd9c130999289b05d19a62d2b84d
zarriy <zarriy@outlook.com>
2023-02-12 15:38:29 +0500

    implemented the mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Filters from "./components/filters";
+import Filters, { MobileFilter } from "./components/filters";
 import ImageCard from "./components/imageCard";
 import { data } from "./context/data";
 import { useContext } from "react";
@@ -14,15 +14,20 @@ function App() {
 
   return (
     <div className="bg-[#f2f3f6] pb-20 min-h-screen">
-      <h2 className="text-3xl text-center text-neutral-800 py-20">
+      <h2 className="text-3xl text-center text-neutral-800 py-10 sm:py-20">
         Filter Products App
       </h2>
-      <div className="flex space-x-4 ">
-        <div className="w-60 p-4 shrink-0">
-          <h3 className="mb-4">Filter by Categories:</h3>
-          <Filters />
+      <div className="sm:flex space-x-4 ">
+        <div className="sm:w-60 p-4 shrink-0 mb-6">
+          <h3 className="mb-4 hidden sm:block">Filter by Categories:</h3>
+          <div className="hidden sm:block">
+            <Filters />
+          </div>
+          <div className="sm:hidden flex justify-end">
+            <MobileFilter />
+          </div>
         </div>
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex gap-2 flex-wrap justify-center">
           {filteredData.map((item) => (
             <ImageCard key={item.id} img={item.img} id={item.id} />
           ))}
--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { useContext } from "react";
 import { FilterContext } from "../context/context";
+import filterImage from "../assets/filter-outline.svg";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+const filters = ["ALL", "Window", "Door", "House"];
 
 export default function Filters() {
   const { handleFilterChange, selected } = useContext(FilterContext);
 
-  const filters = ["ALL", "Window", "Door", "House"];
-
   return (
-    <div className="flex space-y-2 flex-col">
+    <div className="flex space-x-4 sm:space-y-4 sm:flex-col">
       {filters.map((filter, i) => (
         <div
           key={i}
@@ -20,6 +24,61 @@ export default function Filters() {
           {filter}
         </div>
       ))}
+    </div>
+  );
+}
+
+export function MobileFilter() {
+  const [show, setShow] = useState(false);
+  const { handleFilterChange, selected } = useContext(FilterContext);
+  const filterRef = useRef(null);
+
+  useEffect(() => {
+    const outsideClickHandler = (e) => {
+      if (filterRef.current && !filterRef.current.contains(e.target)) {
+        setShow(false);
+      }
+    };
+
+    document.addEventListener("click", outsideClickHandler);
+
+    return () => document.removeEventListener("click", outsideClickHandler);
+  }, [filterRef]);
+
+  const handleClick = (e) => {
+    handleFilterChange(e);
+    setShow(false);
+  };
+
+  return (
+    <div className="flex items-center gap-2 relative" ref={filterRef}>
+      Filter:
+      <div
+        className="w-10 h-10 rounded-lg bg-white flex items-center justify-center cursor-pointer"
+        onClick={() => setShow((prev) => !prev)}
+      >
+        <img
+          src={filterImage}
+          alt="filter Icon"
+          className={`w-6 ${
+            show && "rotate-180"
+          } transition-transform duration-200 ease-out`}
+        />
+      </div>
+      {show && (
+        <ul className="absolute top-10 right-2 flex gap-2 flex-col z-10 bg-white px-6 py-6 w-40 rounded-lg">
+          {filters.map((filter, i) => (
+            <li
+              className={`cursor-pointer px-4 py-2 rounded-lg  hover:shadow-lg ${
+                selected === filter.toLowerCase() && "bg-blue-400"
+              }`}
+              onClick={handleClick}
+            >
+              {filter}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
